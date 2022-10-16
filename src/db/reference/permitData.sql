474ee56949399d33388ac75b461473bc1c6631d2
DrHuangMHT <DrHuangMHT@outlook.com>
2022-10-16 10:10:29 +0800

    make primary key serial
    make primary key serial to improve query performance

--- src/db/reference/permitData.sql
+++ src/db/reference/permitData.sql
@@ -1,13 +1,12 @@
 CREATE TABLE "permitData"(
-    "id" VARCHAR(8) NOT NULL PRIMARY KEY,
+    "id" BIGSERIAL PRIMARY KEY,
     "source" text [] NOT NULL
 );
 
 INSERT INTO
-    "permitData"("id", "source")
+    "permitData"("source")
 VALUES
 (
-        'testPermitId',
         '{
         "permit1.png",
         "permit2.png",
